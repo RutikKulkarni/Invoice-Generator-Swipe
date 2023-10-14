fe94581c51ee5e6944686b1307a408ccd0c92fed
Rutik Kulkarni <86470947+RutikKulkarni@users.noreply.github.com>
2023-10-14 15:26:59 +0530

    Delete src directory

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import { invoiceReducer } from "./invoiceReducer";
-
-const store = createStore(
-  invoiceReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-export default store;
